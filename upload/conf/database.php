9dac18e8573c160ba19cba508aae9e86e7c11887
wz <wudigod12@gmail.com>
2014-10-14 14:47:09 +0800

    '1'

---
+++ upload/conf/database.php
@@ -0,0 +1,11 @@
+<?php
+
+$_CONF["database"] = array(
+	"type"		=> "mysql",		//database type, just mysql supported for the moment
+	"server"	=> "localhost",		//mysql server or socket file
+	"login"		=> "root",		//mysql username
+	"password"	=> "",		//mysql password
+	"default"	=> "12",	//database name
+);
+
+?>
